16a0677b1def1dbb487558e28ea5898341f7a0af
石川幹晴 <kansei2@kansei2.local>
2022-01-02 00:09:14 +0900

    wid odds get

--- wide_odds_collect.py
+++ wide_odds_collect.py
@@ -1,4 +1,5 @@
 import sekitoba_library as lib
+import sekitoba_data_manage as dm
 
 import requests
 import time
@@ -20,30 +21,9 @@ def post_connect( req ):
 
     return None
 
-def web_check( soup ):
-    title = soup.find( "title" ).text.replace( "　", "" )
-
-    if title == "パラメータエラーJRA":
-        return False
-
-    return True
-
-def jra_id_get( race_id ):
-    base_name = "pw155ou10"
-    year = race_id[0:4]
-    race_place_num = race_id[4:6] #place
-    num = race_id[6:8] #回
-    day = race_id[8:10] #日
-    race = race_id[10:12] #?R
-    jra_id = base_name + race_place_num + year + num + day + race + year
-    print( jra_id )
-
-def main():
-    race_id = "202106050901"
-    jra_id_get( race_id )
-    return
+def req_create( cname ):
     url = "https://www.jra.go.jp/JRADB/accessO.html"
-    values = { "cname": "pw155ou10 06 2021 05 07 01 2021 1225 Z/5E" }#place_num 2021 5回7日 R 2021 12/25
+    values = { "cname": cname }#place_num 2021 5回7日 R 2021 12/25
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii') # data should be bytes
     headers = { "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -66,12 +46,39 @@ def main():
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36" }
     
     req = urllib.request.Request(url, data, headers )
-    res = post_connect( req )
-    data = gzip.decompress( res.read() )
-    html_text = data.decode( 'shift_jis' )
-    soup = BeautifulSoup( html_text, "html.parser" )
-    web_check( soup )
-    return
+
+    return req
+
+def web_check( soup ):
+    title = soup.find( "title" ).text.replace( "　", "" )
+
+    if title == "パラメータエラーJRA":
+        return False
+
+    return True
+
+def jra_id_get( race_id, ymd ):
+    base_name = "pw155ou10"
+    year = race_id[0:4]
+    race_place_num = race_id[4:6] #place
+    num = race_id[6:8] #回
+    race_day = race_id[8:10] #日
+    race = race_id[10:12] #?R
+
+    if ymd["month"] < 10:
+        month = "0" + str( int( ymd["month"] ) )
+    else:
+        month = str( int( ymd["month"] ) )
+
+    if ymd["day"] < 10:
+        day = "0" + str( int( ymd["day"] ) )
+    else:
+        day = str( int( ymd["day"] ) )
+
+    jra_id = base_name + race_place_num + year + num + race_day + race + year + month + day + "Z/"
+    return jra_id
+
+def data_collect( soup ):
     result = {}
     ul_tag = soup.findAll( "ul" )    
     
@@ -111,9 +118,53 @@ def main():
 
                     result[horce_num1][horce_num2] = instance
                             
+    return result
 
-    for k in result.keys():
-        print( result[k] )
-            
+def main():
+    result = dm.pickle_load( "wide_odds_data.pickle")
+    race_day = dm.pickle_load( "race_day.pickle" )
+    race_data = dm.pickle_load( "race_data.pickle" )
+
+    if result == None:
+        result = {}
+
+    for k in race_data.keys():
+        race_id = lib.id_get( k )
+        
+        try:
+            a = result[race_id]            
+            continue
+        except:
+            check = False
+        
+        try:
+            base_cname = jra_id_get( race_id, race_day[race_id] )
+        except:
+            continue
+
+        for i in range( 0, 256 ):
+            d = hex(i)[2:].upper()
+
+            if len( d ) == 1:
+                continue
+        
+            cname = base_cname + d
+            req = req_create( cname )
+            res = post_connect( req )
+            data = gzip.decompress( res.read() )
+            html_text = data.decode( 'shift_jis' )
+            soup = BeautifulSoup( html_text, "html.parser" )
+    
+            if web_check( soup ):
+                check = True
+                break
+
+        if check:
+            result[race_id] = data_collect( soup )
+            print( "success {}".format( race_id ) )
+        else:
+            print( "error {}".format( race_id ) )
+
+        lib.dm.pickle_upload( "wide_odds_data.pickle", result )
    
 main()
